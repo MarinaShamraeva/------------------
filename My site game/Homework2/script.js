0d0d91581c7ca939bd99dc1153b2b1cc15625466
Marina <shamraevam@bk.ru>
2025-01-23 00:45:38 +0300

    дз 2 подготовлено к сдаче на проверку от 23.01.2025г

---
+++ My site game/Homework2/script.js
@@ -0,0 +1,94 @@
+// Задание 1
+let a = 10;
+alert(a);
+a = 20; 
+alert(a);
+
+// Задание 2
+const iPhoneRelease = 2007;
+alert(iPhoneRelease);
+// iPhoneRelease = prompt (`Год выпуска первого iPhone`);
+
+// Задание 3
+const javascriptCreator = "Brendan";
+// javascriptCreator = prompt (`имя создателя языка JavaScript`);
+alert(javascriptCreator);
+
+// Задание 4
+let greenApple = 10;
+let redApple = 2;
+
+let sum = 2 + 10;
+//  let apple = prompt (`Сколько всего яблок`);
+alert(sum);
+
+let difference = 10 - 2;
+// apple = prompt (`На сколько красных яблок больше чем зеленых`);
+alert(difference);
+
+let product = 10 * 2;
+// apple = prompt (`Cколько красных яблок будет если увеличить в 2 раза`);
+alert(product);
+
+let quotient = 10 / 2;
+// apple = prompt (`Cколько красных яблок будет если уменьшить в 2 раза`);
+alert(quotient);
+
+// Задание 5
+let meaningOne = 2;
+let meaningTwo = 5;
+
+let power = 2 ** 5;
+// meaning = prompt (`Цифру 2 возвести в степень 5`);
+alert(`Результат задания номер 5 равен ${power}`);
+
+// Задание 6
+let one = 9;
+let two = 2;
+let remainder = 9 % 2; 
+// result = prompt (`Найти остаток от деления 9 на 2`);
+alert(remainder);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+// Задание 8
+let age = prompt('Сколько Вам лет?');
+alert(age);
+
+// Задание 9
+const user= {
+name: 'Максим',
+age: 9,
+isAdmin: false
+}
+
+// Задание 10
+ let userName = prompt('Как Вас зовут?');
+ alert(`Привет ${userName}!`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
